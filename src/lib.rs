8ed763a916c2bd571667d0677d1124534b798230
Roland Baumann <r.dddib.b@gmail.com>
2016-12-16 03:29:58 -0500

    Corrected Relation equality

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Link<A, B> {
     from: A,
     to: B,
@@ -11,6 +11,13 @@ impl<A: PartialEq, B: PartialEq> PartialEq for Link<A, B> {
     }
 }
 
+impl<A: PartialEq, B: PartialEq> Link<A, B> {
+
+    pub fn new(f: A, t: B) -> Link<A, B> {
+        Link { from: f, to: t}
+    }
+}
+
 #[derive(Debug)]
 pub struct Relation<'a, A: 'a, B: 'a> {
     links: &'a mut Vec<Link<A, B>>,
@@ -19,24 +26,34 @@ pub struct Relation<'a, A: 'a, B: 'a> {
 impl<'a, A: PartialEq + 'a, B: PartialEq + 'a> PartialEq for Relation<'a, A, B> {
 
     fn eq(&self, other: &Relation<'a, A, B>) -> bool {
-        let ref this = *self.links;
-        let ref that = *other.links;
-        (this.len() == that.len()) &&
-            this.iter().zip(that).all(|(a, b)| a == b)
+        let ref a = *self.links;
+        if self.links.len() != other.links.len() { return false; }
+        for item in a {
+            if !other.has(item) { return false; }
+        }
+        return true;
     }
 }
 
-impl<'a, A: 'a, B: 'a> Relation<'a, A, B> {
+impl<'a, A: PartialEq + 'a, B: PartialEq + 'a> Relation<'a, A, B> {
 
     pub fn add_link(&mut self, l: Link<A, B>) {
-        self.links.push(l);
+        if !self.has(&l) {
+            self.links.push(l);
+        }
     }
 
     pub fn add_links(&mut self, ls: Vec<(A, B)>) {
         for l in ls {
-            let link = Link { from: l.0, to: l.1 };
-            self.add_link(link);
+            let link = Link::new(l.0, l.1);
+            if !self.has(&link) {
+                self.add_link(link);
+            }
         }
+    }
+
+    pub fn has(&self, l: &Link<A, B>) -> bool {
+        self.links.iter().any(|x| x == l)
     }
 }
 
@@ -46,23 +63,36 @@ mod tests {
 
     #[test]
     fn test_link_eq() {
-        assert_eq!(Link { from: 0, to: 1}, Link { from: 0, to: 1 });
-        assert_ne!(Link { from: 0, to: 1}, Link { from: 0, to: 2 });
-        assert_ne!(Link { from: 1, to: 1}, Link { from: 0, to: 1 });
+        assert_eq!(Link::new(0, 1), Link::new(0, 1));
+        assert_ne!(Link::new(0, 1), Link::new(0, 2));
+        assert_ne!(Link::new(1, 1), Link::new(0, 1));
     }
 
     #[test]
     fn test_relation_eq() {
-        assert_eq!(Relation { links: &mut vec![Link { from: 0, to: 1}] }, Relation { links: &mut vec![Link { from: 0, to: 1}] });
-        assert_ne!(Relation { links: &mut vec![Link { from: 0, to: 1}] }, Relation { links: &mut vec![Link { from: 0, to: 2}] });
-        assert_ne!(Relation { links: &mut vec![Link { from: 0, to: 2}] }, Relation { links: &mut Vec::new() });
+        assert_eq!(Relation { links: &mut vec![Link::new(0, 1)] }, Relation { links: &mut vec![Link::new(0, 1)] });
+        assert_ne!(Relation { links: &mut vec![Link::new(0, 1)] }, Relation { links: &mut vec![Link::new(0, 2)] });
+        assert_ne!(Relation { links: &mut vec![Link::new(0, 2)] }, Relation { links: &mut Vec::new() });
+
+        let r1 = Relation { links: &mut vec![Link::new(1, 0), Link::new(0, 1)] };
+        let r2 = Relation { links: &mut vec![Link::new(0, 1), Link::new(1, 0)] };
+        assert_eq!(r1, r2);
     }
 
     #[test]
     fn test_link() {
         let mut a = Relation { links: &mut Vec::new() };
-        let b = Relation { links: &mut vec![Link { from: 0, to: 1 }] };
-        a.add_link(Link { from: 0, to: 1 });
+        let b = Relation { links: &mut vec![Link::new(0, 1)] };
+        a.add_link(Link::new(0, 1));
+        a.add_link(Link::new(0, 1));
         assert_eq!(b, a);
     }
+
+    #[test]
+    fn test_has() {
+        let a = Relation { links: &mut vec![Link::new(0, 1)] };
+        assert!(a.has(&Link::new(0, 1)));
+        assert!(!a.has(&Link::new(1, 1)));
+    }
+
 }
