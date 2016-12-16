27afd92122a828965e85296c94c0caa636e70a0f
Roland Baumann <r.dddib.b@gmail.com>
2016-12-16 04:21:45 -0500

    Removed the Link type and began adding domain methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,9 @@
-#[derive(Debug, Clone)]
-pub struct Link<A, B> {
-    from: A,
-    to: B,
-}
-
-impl<A: PartialEq, B: PartialEq> PartialEq for Link<A, B> {
-
-    fn eq(&self, other: &Link< A, B>) -> bool {
-        (self.from == other.from) & (self.to == other.to)
-    }
-}
-
-impl<A: PartialEq, B: PartialEq> Link<A, B> {
-
-    pub fn new(f: A, t: B) -> Link<A, B> {
-        Link { from: f, to: t}
-    }
-}
-
 #[derive(Debug)]
 pub struct Relation<'a, A: 'a, B: 'a> {
-    links: &'a mut Vec<Link<A, B>>,
+    links: &'a mut Vec<(A, B)>,
 }
 
-impl<'a, A: PartialEq + 'a, B: PartialEq + 'a> PartialEq for Relation<'a, A, B> {
+impl<'a, A: Ord + Clone + 'a, B: Ord + Clone + 'a> PartialEq for Relation<'a, A, B> {
 
     fn eq(&self, other: &Relation<'a, A, B>) -> bool {
         let ref a = *self.links;
@@ -35,9 +15,9 @@ impl<'a, A: PartialEq + 'a, B: PartialEq + 'a> PartialEq for Relation<'a, A, B> 
     }
 }
 
-impl<'a, A: PartialEq + 'a, B: PartialEq + 'a> Relation<'a, A, B> {
+impl<'a, A: Ord + Clone + 'a, B: Ord + Clone + 'a> Relation<'a, A, B> {
 
-    pub fn add_link(&mut self, l: Link<A, B>) {
+    pub fn add_link(&mut self, l: (A, B)) {
         if !self.has(&l) {
             self.links.push(l);
         }
@@ -45,15 +25,28 @@ impl<'a, A: PartialEq + 'a, B: PartialEq + 'a> Relation<'a, A, B> {
 
     pub fn add_links(&mut self, ls: Vec<(A, B)>) {
         for l in ls {
-            let link = Link::new(l.0, l.1);
-            if !self.has(&link) {
-                self.add_link(link);
+            if !self.has(&l) {
+                self.add_link(l);
             }
         }
     }
 
-    pub fn has(&self, l: &Link<A, B>) -> bool {
+    pub fn has(&self, l: &(A, B)) -> bool {
         self.links.iter().any(|x| x == l)
+    }
+
+    pub fn domain(&self) -> Vec<A> {
+        let mut a = self.links.iter().map(|x| x.0.clone()).collect::<Vec<A>>();
+        a.sort();
+        a.dedup();
+        a
+    }
+
+    pub fn codomain(&self) -> Vec<B> {
+        let mut a = self.links.iter().map(|x| x.1.clone()).collect::<Vec<B>>();
+        a.sort();
+        a.dedup();
+        a
     }
 }
 
@@ -63,36 +56,49 @@ mod tests {
 
     #[test]
     fn test_link_eq() {
-        assert_eq!(Link::new(0, 1), Link::new(0, 1));
-        assert_ne!(Link::new(0, 1), Link::new(0, 2));
-        assert_ne!(Link::new(1, 1), Link::new(0, 1));
+        assert_eq!((0, 1), (0, 1));
+        assert_ne!((0, 1), (0, 2));
+        assert_ne!((1, 1), (0, 1));
     }
 
     #[test]
     fn test_relation_eq() {
-        assert_eq!(Relation { links: &mut vec![Link::new(0, 1)] }, Relation { links: &mut vec![Link::new(0, 1)] });
-        assert_ne!(Relation { links: &mut vec![Link::new(0, 1)] }, Relation { links: &mut vec![Link::new(0, 2)] });
-        assert_ne!(Relation { links: &mut vec![Link::new(0, 2)] }, Relation { links: &mut Vec::new() });
+        assert_eq!(Relation { links: &mut vec![(0, 1)] }, Relation { links: &mut vec![(0, 1)] });
+        assert_ne!(Relation { links: &mut vec![(0, 1)] }, Relation { links: &mut vec![(0, 2)] });
+        assert_ne!(Relation { links: &mut vec![(0, 2)] }, Relation { links: &mut Vec::new() });
 
-        let r1 = Relation { links: &mut vec![Link::new(1, 0), Link::new(0, 1)] };
-        let r2 = Relation { links: &mut vec![Link::new(0, 1), Link::new(1, 0)] };
+        let r1 = Relation { links: &mut vec![(1, 0), (0, 1)] };
+        let r2 = Relation { links: &mut vec![(0, 1), (1, 0)] };
         assert_eq!(r1, r2);
     }
 
     #[test]
     fn test_link() {
         let mut a = Relation { links: &mut Vec::new() };
-        let b = Relation { links: &mut vec![Link::new(0, 1)] };
-        a.add_link(Link::new(0, 1));
-        a.add_link(Link::new(0, 1));
+        let b = Relation { links: &mut vec![(0, 1)] };
+        a.add_link((0, 1));
+        a.add_link((0, 1));
         assert_eq!(b, a);
     }
 
     #[test]
     fn test_has() {
-        let a = Relation { links: &mut vec![Link::new(0, 1)] };
-        assert!(a.has(&Link::new(0, 1)));
-        assert!(!a.has(&Link::new(1, 1)));
+        let a = Relation { links: &mut vec![(0, 1)] };
+        assert!(a.has(&(0, 1)));
+        assert!(!a.has(&(1, 1)));
     }
 
+    #[test]
+    fn test_domain() {
+        let mut a = Relation { links: &mut vec![(0, 1)] };
+        a.add_links(vec![(1, 2), (2, 2), (3, 5)]);
+        assert_eq!(a.domain(), vec![0, 1, 2, 3]);
+    }
+
+    #[test]
+    fn test_codomain() {
+        let mut a = Relation { links: &mut vec![(0, 1)] };
+        a.add_links(vec![(1, 2), (2, 2), (3, 5)]);
+        assert_eq!(a.codomain(), vec![1, 2, 5]);
+    }
 }
