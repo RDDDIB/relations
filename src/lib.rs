3daacdbbbcccd3fe7c487d8dbe40a9fe7c2ea4a5
Roland Baumann <r.dddib.b@gmail.com>
2016-12-17 04:27:35 -0500

    Added Sets

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,52 @@
 #[derive(Debug)]
-pub struct Relation<'a, A: 'a, B: 'a> {
-    links: &'a mut Vec<(A, B)>,
+pub struct Set<A> {
+    items: Vec<A>,
 }
 
-impl<'a, A: Ord + Clone + 'a, B: Ord + Clone + 'a> PartialEq for Relation<'a, A, B> {
+impl<A: Ord + Clone> PartialEq for Set<A> {
 
-    fn eq(&self, other: &Relation<'a, A, B>) -> bool {
+    fn eq(&self, other: &Set<A>) -> bool {
+        let ref a = *self.items;
+        if self.items.len() != other.items.len() { return false; }
+        for item in a {
+            if !other.has(item) { return false; }
+        }
+        return true;
+    }
+}
+
+impl<A: Ord + Clone> Set<A> {
+
+    pub fn has(&self, l: &A) -> bool {
+        self.items.iter().any(|x| x == l)
+    }
+
+    pub fn union(&self, other: &Set<A>) -> Set<A> {
+        let mut v = Vec::new();
+        v.extend_from_slice(self.items.as_slice());
+        v.extend_from_slice(other.items.as_slice());
+        v.sort();
+        v.dedup();
+        Set { items: v }
+    }
+
+    pub fn inter(&self, other: &Set<A>) -> Set<A> {
+        let mut v = Vec::new();
+        for item in self.items.iter() {
+            if other.has(&item) { v.push(item.clone()); }
+        }
+        Set { items: v }
+    }
+}
+
+#[derive(Debug)]
+pub struct Relation<'a, A: 'a> {
+    links: &'a mut Vec<(A, A)>,
+}
+
+impl<'a, A: Ord + Clone + 'a> PartialEq for Relation<'a, A> {
+
+    fn eq(&self, other: &Relation<'a, A>) -> bool {
         let ref a = *self.links;
         if self.links.len() != other.links.len() { return false; }
         for item in a {
@@ -15,15 +56,15 @@ impl<'a, A: Ord + Clone + 'a, B: Ord + Clone + 'a> PartialEq for Relation<'a, A,
     }
 }
 
-impl<'a, A: Ord + Clone + 'a, B: Ord + Clone + 'a> Relation<'a, A, B> {
+impl<'a, A: Ord + Clone + 'a> Relation<'a, A> {
 
-    pub fn add_link(&mut self, l: (A, B)) {
+    pub fn add_link(&mut self, l: (A, A)) {
         if !self.has(&l) {
             self.links.push(l);
         }
     }
 
-    pub fn add_links(&mut self, ls: Vec<(A, B)>) {
+    pub fn add_links(&mut self, ls: Vec<(A, A)>) {
         for l in ls {
             if !self.has(&l) {
                 self.add_link(l);
@@ -31,7 +72,7 @@ impl<'a, A: Ord + Clone + 'a, B: Ord + Clone + 'a> Relation<'a, A, B> {
         }
     }
 
-    pub fn has(&self, l: &(A, B)) -> bool {
+    pub fn has(&self, l: &(A, A)) -> bool {
         self.links.iter().any(|x| x == l)
     }
 
@@ -42,8 +83,8 @@ impl<'a, A: Ord + Clone + 'a, B: Ord + Clone + 'a> Relation<'a, A, B> {
         a
     }
 
-    pub fn codomain(&self) -> Vec<B> {
-        let mut a = self.links.iter().map(|x| x.1.clone()).collect::<Vec<B>>();
+    pub fn codomain(&self) -> Vec<A> {
+        let mut a = self.links.iter().map(|x| x.1.clone()).collect::<Vec<A>>();
         a.sort();
         a.dedup();
         a
@@ -53,6 +94,22 @@ impl<'a, A: Ord + Clone + 'a, B: Ord + Clone + 'a> Relation<'a, A, B> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_union() {
+        let a = Set { items: vec![0, 1, 2, 3, 4, 5] };
+        let b = Set { items: vec![4, 5, 6, 7, 8, 9] };
+        let c = Set { items: vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9] };
+        assert_eq!(a.union(&b), c);
+    }
+
+    #[test]
+    fn test_inter() {
+        let a = Set { items: vec![0, 1, 2, 3, 4, 5] };
+        let b = Set { items: vec![4, 5, 6, 7, 8, 9] };
+        let c = Set { items: vec![4, 5] };
+        assert_eq!(a.inter(&b), c);
+    }
 
     #[test]
     fn test_link_eq() {
